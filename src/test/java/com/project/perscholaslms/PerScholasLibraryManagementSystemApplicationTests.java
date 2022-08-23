d7104b8c090c58dc8401c8512425378ef147cc08
vrvimal <vrvprasath@gmail.com>
2022-08-23 11:54:40 -0400

    Files Added

--- src/test/java/com/project/perscholaslms/PerScholasLibraryManagementSystemApplicationTests.java
+++ src/test/java/com/project/perscholaslms/PerScholasLibraryManagementSystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PerScholasLibraryManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
