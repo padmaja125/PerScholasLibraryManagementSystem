d076f638a0240c71719e989aed4a49064c999f26
vrvimal <vrvprasath@gmail.com>
2022-08-23 10:08:00 -0400

    first commit

---
+++ src/main/java/com/project/perscholaslms/PerScholasLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.perscholaslms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerScholasLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerScholasLibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/perscholaslms/PerScholasLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.perscholaslms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerScholasLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
